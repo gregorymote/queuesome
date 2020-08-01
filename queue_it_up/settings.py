6717f1a70d5cd0b27cf0a66aff047a67988fd82b
gregorymote <greg.mote11@gmail.com>
2020-08-01 07:02:51 -0400

    set to prod

--- queue_it_up/settings.py
+++ queue_it_up/settings.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 from selenium import webdriver
 
-PROD = False
+PROD = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
