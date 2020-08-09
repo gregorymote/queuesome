e70dfbf7f416bb6916bbc6eaceaa27207b92362a
gregorymote <greg.mote11@gmail.com>
2020-08-09 11:00:05 -0400

    debugging

--- queue_it_up/settings.py
+++ queue_it_up/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_Q")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = PROD
-ALLOWED_HOSTS = ['localhost', 'q-it-up.herokuapp.com', '.qitup.us', 'qitup.us']
+ALLOWED_HOSTS = ['localhost', 'q-it-up.herokuapp.com', 'www.qitup.us']
 DEBUG_PROPAGATE_EXCEPTIONS = False
 
 # Application definition
