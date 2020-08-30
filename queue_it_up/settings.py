fba6f2b8a52ee2c8f8eb970b51db4b5d75e9383b
gregorymote <greg.mote11@gmail.com>
2020-08-30 10:36:15 -0400

    debugr

--- queue_it_up/settings.py
+++ queue_it_up/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET_Q")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = not PROD
+DEBUG = True
 ALLOWED_HOSTS = ['localhost', 'q-it-up.herokuapp.com', 'www.qitup.us', 'www.friyayvibes.com']
 DEBUG_PROPAGATE_EXCEPTIONS = False
 
