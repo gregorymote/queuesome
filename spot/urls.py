0bd8a09fc7af7ac2a28c9206ca6d714e4ea06bb2
gregorymote <greg.mote11@gmail.com>
2024-02-03 14:46:36 -0500

    modify path storage, begin time capture in db

--- spot/urls.py
+++ spot/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("start", views.start, name="start"),
     path("studio", views.studio, name="studio"),
     path('update_play', views.update_play, name='update_play'),
-    path('get_path', views.get_path, name="get_path")
+    path('get_path', views.get_path, name="get_path"),
+    path('set_start', views.set_start, name="set_start")
 ]
