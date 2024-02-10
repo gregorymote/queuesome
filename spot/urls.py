6d5ff53a060ae578991c20d38ee1646ea8539123
gregorymote <greg.mote11@gmail.com>
2024-02-10 16:55:18 -0500

    add live editing to studio

--- spot/urls.py
+++ spot/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("login", views.login, name="login"),
     path("studio/<int:pid>/", views.studio, name="studio"),
     path('update_play', views.update_play, name='update_play'),
+    path('update_search', views.update_search, name="update_search"),
     path('get_path', views.get_path, name="get_path"),
     path('set_start', views.set_start, name="set_start")
 ]
