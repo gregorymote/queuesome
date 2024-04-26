13bfce98028061809c9e7d4827ff8f33f6b051f0
gregorymote <greg.mote11@gmail.com>
2024-04-26 18:04:42 -0400

    debug ssl cert issue

--- queue_it_up/settings.py
+++ queue_it_up/settings.py
@@ -24,7 +24,7 @@ elif STATE == "STAGE":
     DEBUG = True
 else:
     HEROKU = True
-    DEBUG = False
+    DEBUG = True
     STAGE = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
